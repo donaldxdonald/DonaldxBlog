78644a39f00fbb0709cf0ea236ab1be20c647cc2
Donald Mok <mzhlovefama@gmail.com>
2020-06-04 01:50:52 +0800

    what have i modified?

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+import './styles.scss'
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VideoBackground from 'vue-responsive-video-background-player'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    VideoBackground
   }
 }
 </script>
